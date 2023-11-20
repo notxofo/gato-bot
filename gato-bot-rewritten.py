1b3c622b73b9257d7196ec00775cff4337c901fb
Xofo <147416971+notxofo@users.noreply.github.com>
2023-11-20 08:55:07 +0000

    Update gato-bot-rewritten.py

--- gato-bot-rewritten.py
+++ gato-bot-rewritten.py
@@ -1,6 +1,7 @@
 import discord
 import json
 import asyncio
+import random
 
 with open('config.json') as config_file:
   data = json.load(config_file)
@@ -17,6 +18,12 @@ async def ban(ctx, user: discord.User, reason: str = "No reason provided"):
   guild = ctx.guild
   await guild.ban(user, reason=reason)
   await ctx.send(f"{user.name} has been banned from the server. Reason: {reason}")
+
+@bot.slash_command(name="unban", description="Unbans a user from the server", permissions=["ban_members"])
+async def unban(ctx, user: discord.User):
+  guild = ctx.guild
+  await guild.unban(user)
+  await ctx.send(f"{user.name} has been unbanned from the server.")
 
 @bot.slash_command(name="kick", description="Kicks the selected user from the server", permissions=["kick_members"])
 async def kick(ctx, user: discord.User, reason: str = "No reason provided"):
@@ -45,5 +52,47 @@ async def avatar(ctx, user: discord.User):
 async def purge(ctx, amount: int):
   await ctx.channel.purge(limit=amount)
   await ctx.send(f"Purged {amount} messages!")
+
+@bot.slash_command(name="serverinfo", description="Displays information about the server", permissions=["manage_guild"])
+async def serverinfo(ctx):
+  guild = ctx.guild
+  name = guild.name
+  description = guild.description
+  owner = guild.owner
+  id = guild.id
+  region = guild.region
+  member_count = guild.member_count
+  icon = guild.icon_url
+  embed = discord.Embed(title=name, description=description, color=discord.Color.blue())
+  embed.set_thumbnail(url=icon)
+  embed.add_field(name="Owner", value=owner, inline=True)
+  embed.add_field(name="Server ID", value=id, inline=True)
+  embed.add_field(name="Region", value=region, inline=True)
+  embed.add_field(name="Member Count", value=member_count, inline=True)
+  await ctx.send(embed=embed)
+
+@bot.slash_command(name="8ball", description="Answers a yes/no question")
+async def eight_ball(ctx, question: str):
+  responses = ["It is certain.",
+               "It is decidedly so.",
+               "Without a doubt.",
+               "Yes - definitely.",
+               "You may rely on it.",
+               "As I see it, yes.",
+               "Most likely.",
+               "Outlook good.",
+               "Yes.",
+               "Signs point to yes.",
+               "Reply hazy, try again.",
+               "Ask again later.",
+               "Better not tell you now.",
+               "Cannot predict now.",
+               "Concentrate and ask again.",
+               "Don't count on it.",
+               "My reply is no.",
+               "My sources say no.",
+               "Outlook not so good.",
+               "Very doubtful."]
+  await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
 
 bot.run(token)
